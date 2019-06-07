938dd55ad72c7dfac4c0452d105b00235b926472
CrookedFingerGuy <39472072+CrookedFingerGuy@users.noreply.github.com>
2019-06-07 15:33:11 -0700

    Perspective correction, Rotating and Cropping are working in the easy 4 point contour cses.

--- 3Dekeystoner/Form1.cs
+++ 3Dekeystoner/Form1.cs
@@ -72,9 +72,31 @@ namespace _3Dekeystoner
             CvInvoke.ApproxPolyDP(convexCOI[0], convexCOI[0], 100, true);
             CvInvoke.DrawContours(lineImage, convexCOI, 0, new MCvScalar(0.0), 3);
 
+            Mat pMatrix = new Mat();
+            PointF[] temp = new PointF[] { new PointF(2048, 0), new PointF(2048, 2048), new PointF(0, 2048), new PointF(0, 0) };
+
+            
+            if (convexCOI[0].Size == 4)
+            {
+                Point[] ctemp = convexCOI[0].ToArray();
+                PointF[] coit = new PointF[4];
+                for (int i = 0; i < 4; i++)
+                {
+                    coit[i].X = ctemp[i].X;
+                    coit[i].Y = ctemp[i].Y;
+                }         
+
+                pMatrix = CvInvoke.GetPerspectiveTransform(coit, temp);
+                CvInvoke.WarpPerspective(colorImage, colorImage, pMatrix, colorImage.Size);
+            }
+
+            Mat croped = new Mat(colorImage, new Rectangle(0, 0, 2048, 2048));
+            CvInvoke.GetRotationMatrix2D(new Point(croped.Cols / 2, croped.Rows / 2), -90, 1, pMatrix);
+            CvInvoke.WarpAffine(croped, croped, pMatrix, croped.Size);
+
 
             lineImageBox.Image = lineImage;            
-            finalImageBox.Image = colorImage;
+            finalImageBox.Image = croped;
 
             this.Text = convexCOI[0].Size.ToString();
         }
