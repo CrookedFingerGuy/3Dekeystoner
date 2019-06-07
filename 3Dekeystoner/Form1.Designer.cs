d191aa47311185d1e71fdee39f6aecc67639d6d8
CrookedFingerGuy <39472072+CrookedFingerGuy@users.noreply.github.com>
2019-06-06 19:15:46 -0700

    Largest Contour Finding Works

--- 3Dekeystoner/Form1.Designer.cs
+++ 3Dekeystoner/Form1.Designer.cs
@@ -31,27 +31,57 @@
             this.components = new System.ComponentModel.Container();
             this.finalImageBox = new Emgu.CV.UI.ImageBox();
             this.lineImageBox = new Emgu.CV.UI.ImageBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.actionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nextContourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.finalImageBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.lineImageBox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // triangleRectangleImageBox
+            // finalImageBox
             // 
-            this.finalImageBox.Location = new System.Drawing.Point(12, 12);
-            this.finalImageBox.Name = "triangleRectangleImageBox";
-            this.finalImageBox.Size = new System.Drawing.Size(643, 827);
+            this.finalImageBox.Location = new System.Drawing.Point(12, 27);
+            this.finalImageBox.Name = "finalImageBox";
+            this.finalImageBox.Size = new System.Drawing.Size(643, 812);
             this.finalImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.finalImageBox.TabIndex = 2;
             this.finalImageBox.TabStop = false;
             // 
             // lineImageBox
             // 
-            this.lineImageBox.Location = new System.Drawing.Point(661, 12);
+            this.lineImageBox.Location = new System.Drawing.Point(661, 27);
             this.lineImageBox.Name = "lineImageBox";
-            this.lineImageBox.Size = new System.Drawing.Size(676, 827);
+            this.lineImageBox.Size = new System.Drawing.Size(676, 812);
             this.lineImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.lineImageBox.TabIndex = 2;
             this.lineImageBox.TabStop = false;
+            this.lineImageBox.Click += new System.EventHandler(this.lineImageBox_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.actionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1349, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // actionsToolStripMenuItem
+            // 
+            this.actionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nextContourToolStripMenuItem});
+            this.actionsToolStripMenuItem.Name = "actionsToolStripMenuItem";
+            this.actionsToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.actionsToolStripMenuItem.Text = "Actions";
+            // 
+            // nextContourToolStripMenuItem
+            // 
+            this.nextContourToolStripMenuItem.Name = "nextContourToolStripMenuItem";
+            this.nextContourToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nextContourToolStripMenuItem.Text = "Next Contour";
+            this.nextContourToolStripMenuItem.Click += new System.EventHandler(this.nextContourToolStripMenuItem_Click);
             // 
             // Form1
             // 
@@ -60,18 +90,26 @@
             this.ClientSize = new System.Drawing.Size(1349, 851);
             this.Controls.Add(this.lineImageBox);
             this.Controls.Add(this.finalImageBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.finalImageBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.lineImageBox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private Emgu.CV.UI.ImageBox finalImageBox;
         private Emgu.CV.UI.ImageBox lineImageBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem actionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nextContourToolStripMenuItem;
     }
 }
 
--- 3Dekeystoner/Form1.cs
+++ 3Dekeystoner/Form1.cs
@@ -17,6 +17,10 @@ namespace _3Dekeystoner
 {
     public partial class Form1 : Form
     {
+        public Mat lineImage;
+        public VectorOfVectorOfPoint contours;
+        public int contourCount;
+
         public Form1()
         {
             InitializeComponent();
@@ -25,109 +29,70 @@ namespace _3Dekeystoner
         private void Form1_Load(object sender, EventArgs e)
         {
 
-            Mat img =new Mat("C:\\Users\\Chiz\\Documents\\3D Projects\\Wizardry 8\\Front.jpg");
+            Mat img =new Mat("C:\\Users\\Chiz\\Documents\\3D Projects\\Wizardry 8\\Left.jpg");
             UMat uimage = new UMat();
-            CvInvoke.CvtColor(img, uimage, ColorConversion.Bgr2Gray);
-
+            CvInvoke.CvtColor(img, img, ColorConversion.Bgr2Gray);
+            
             UMat pyrDown = new UMat();
-            CvInvoke.PyrDown(uimage, pyrDown);
-            CvInvoke.PyrUp(pyrDown, uimage);
+            CvInvoke.PyrDown(img, pyrDown);
+            CvInvoke.PyrUp(pyrDown, img);
+            CvInvoke.Threshold(img, img, 100, 255, ThresholdType.BinaryInv);
 
-            double cannyThreshold = 180.0;
-            double cannyThresholdLinking = 10.0;
+
+            /*
+            double cannyThreshold = 125;
+            double cannyThresholdLinking = 255;
             UMat cannyEdges = new UMat();
+            //CvInvoke.Canny(img, img, cannyThreshold, cannyThresholdLinking);
+            */
 
-            CvInvoke.Canny(uimage, cannyEdges, cannyThreshold, cannyThresholdLinking);
-
-            VectorOfVectorOfPoint contours=new VectorOfVectorOfPoint();
+            contours =new VectorOfVectorOfPoint();
             Mat m = new Mat();
-            CvInvoke.FindContours(cannyEdges, contours, m, RetrType.External,ChainApproxMethod.ChainApproxSimple);
+            CvInvoke.FindContours(img, contours, m, RetrType.Ccomp,ChainApproxMethod.ChainApproxSimple);
             finalImageBox.Image = img;
 
+            this.Text = contours.Size.ToString();
 
-            Mat lineImage = new Mat();
+            lineImage = new Mat();
             img.CopyTo(lineImage);
-            lineImage.SetTo(new MCvScalar(0.0));
-            for (int i = 0; i < contours.Size; i++)
+            lineImage.SetTo(new MCvScalar(255.0));
+            //CvInvoke.DrawContours(lineImage, contours, -1, new MCvScalar(0.0), 2);
+
+            double largestArea = 0.0;
+            double tempArea = 0.0;
+            int largestContourIndex = 0;
+
+            for(int i=0;i<contours.Size;i++)
             {
-               CvInvoke.DrawContours(lineImage, contours, i, new MCvScalar(255, 0, 0), 2);
+                tempArea= CvInvoke.ContourArea(contours[i]);
+                if(tempArea>largestArea)
+                {
+                    largestArea = tempArea;
+                    largestContourIndex = i;
+                }
             }
+
+            CvInvoke.DrawContours(lineImage, contours, largestContourIndex, new MCvScalar(0.0), 2);
             lineImageBox.Image = lineImage;
-            /*
-            LineSegment2D[] boxEdges = { new LineSegment2D(new Point(0, 0), new Point(0, 0)), new LineSegment2D(new Point(img.Size.Width, 0), new Point(0, 0)), new LineSegment2D(new Point(0, 0), new Point(0, 0)), new LineSegment2D(new Point(0, img.Size.Height), new Point(0, 0)) };
-
-            foreach (LineSegment2D line in lines)
-            {
-                if (line.P1.X > boxEdges[0].P1.X)
-                {
-                    boxEdges[0].P1 = line.P1;
-                }
-
-                if (line.P2.X > boxEdges[0].P1.X)
-                {
-                    boxEdges[0].P1 = line.P2;
-                }
-
-                if (line.P1.X < boxEdges[1].P1.X)
-                {
-                    boxEdges[1].P1 = line.P1;
-                }
-
-                if (line.P2.X < boxEdges[1].P1.X)
-                {
-                    boxEdges[1].P1 = line.P2;
-                }
-
-                if (line.P1.Y > boxEdges[2].P1.Y)
-                {
-                    boxEdges[2].P1 = line.P1;
-                }
-
-                if (line.P2.Y > boxEdges[2].P1.Y)
-                {
-                    boxEdges[2].P1 = line.P2;
-                }
-
-                if (line.P1.Y < boxEdges[3].P1.Y)
-                {
-                    boxEdges[3].P1 = line.P1;
-                }
-
-                if (line.P2.Y < boxEdges[3].P1.Y)
-                {
-                    boxEdges[3].P1 = line.P2;
-                }
-            }
-
-            boxEdges[0].P2 = boxEdges[2].P1;
-            boxEdges[1].P2 = boxEdges[3].P1;
-            boxEdges[2].P2 = boxEdges[1].P1;
-            boxEdges[3].P2 = boxEdges[0].P1;
-            
-
-            Mat matrixMcMatrix = new Mat();
-            Mat perspMat = new Mat();
-
-            
-            Mat lineImage2 = new Mat();
-            perspMat= CvInvoke.GetPerspectiveTransform(
-                new PointF[] { new PointF { X = boxEdges[0].P1.X, Y =boxEdges[0].P1.Y }, new PointF { X = boxEdges[2].P1.X, Y = boxEdges[2].P1.Y },
-                new PointF {X = boxEdges[3].P1.X, Y =boxEdges[3].P1.Y }, new PointF {X = boxEdges[1].P1.X, Y =boxEdges[1].P1.Y } }, 
-                new PointF[] { new PointF { X = 0, Y = 0 }, new PointF { X = 2048, Y = 0 }, new PointF { X = 0, Y = 2048 }, new PointF { X = 2048, Y = 2048 } });
-            CvInvoke.WarpPerspective(lineImage,lineImage2, perspMat, lineImage.Size);
-            */
             Mat matrixMcMatrix = new Mat();
             Mat lineImage2 = lineImage;
             //Mat croped = new Mat(lineImage2, new Rectangle(0, 0, 2048, 2048));
-            CvInvoke.GetRotationMatrix2D(new Point(lineImage2.Cols / 2, lineImage2.Rows / 2), -90, 1, matrixMcMatrix);            
-            CvInvoke.WarpAffine(lineImage2, lineImage2, matrixMcMatrix, lineImage.Size);
-            finalImageBox.Image = lineImage2;
-            
-
-
-
+            //CvInvoke.GetRotationMatrix2D(new Point(lineImage2.Cols / 2, lineImage2.Rows / 2), -90, 1, matrixMcMatrix);            
+            //CvInvoke.WarpAffine(lineImage2, lineImage2, matrixMcMatrix, lineImage.Size);                      
             //foreach (LineSegment2D line in boxEdges)
             //  CvInvoke.Line(lineImage, line.P1, line.P2, new MCvScalar(0, 0, 255), 2);
+        }
+
+        private void lineImageBox_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void nextContourToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            lineImage.SetTo(new MCvScalar(0.0));
+            CvInvoke.DrawContours(lineImage, contours, contourCount, new MCvScalar(255, 0, 0), 2);
+
+            lineImageBox.Image = lineImage;
         }
     }
 }
