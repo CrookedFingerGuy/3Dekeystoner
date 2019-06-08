5bbb6b1b3776acd2d5cf0045efc1e510c81620c2
CrookedFingerGuy <39472072+CrookedFingerGuy@users.noreply.github.com>
2019-06-07 21:28:02 -0700

    ImageBoxes showing each stage of the image proccesing.

--- 3Dekeystoner/Form1.Designer.cs
+++ 3Dekeystoner/Form1.Designer.cs
@@ -29,76 +29,193 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.finalImageBox = new Emgu.CV.UI.ImageBox();
-            this.lineImageBox = new Emgu.CV.UI.ImageBox();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.actionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.nextContourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            ((System.ComponentModel.ISupportInitialize)(this.finalImageBox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.lineImageBox)).BeginInit();
-            this.menuStrip1.SuspendLayout();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.imageBox3 = new Emgu.CV.UI.ImageBox();
+            this.imageBox4 = new Emgu.CV.UI.ImageBox();
+            this.imageBox5 = new Emgu.CV.UI.ImageBox();
+            this.imageBox6 = new Emgu.CV.UI.ImageBox();
+            this.menuStrip2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox6)).BeginInit();
             this.SuspendLayout();
             // 
-            // finalImageBox
+            // menuStrip2
             // 
-            this.finalImageBox.Location = new System.Drawing.Point(12, 27);
-            this.finalImageBox.Name = "finalImageBox";
-            this.finalImageBox.Size = new System.Drawing.Size(643, 812);
-            this.finalImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.finalImageBox.TabIndex = 2;
-            this.finalImageBox.TabStop = false;
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(1012, 24);
+            this.menuStrip2.TabIndex = 0;
+            this.menuStrip2.Text = "menuStrip2";
             // 
-            // lineImageBox
+            // fileToolStripMenuItem
             // 
-            this.lineImageBox.Location = new System.Drawing.Point(661, 27);
-            this.lineImageBox.Name = "lineImageBox";
-            this.lineImageBox.Size = new System.Drawing.Size(676, 812);
-            this.lineImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.lineImageBox.TabIndex = 2;
-            this.lineImageBox.TabStop = false;
-            this.lineImageBox.Click += new System.EventHandler(this.lineImageBox_Click);
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
             // 
-            // menuStrip1
+            // toolStripMenuItem2
             // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.actionsToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1349, 24);
-            this.menuStrip1.TabIndex = 3;
-            this.menuStrip1.Text = "menuStrip1";
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem2.Text = "&Open...";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
             // 
-            // actionsToolStripMenuItem
+            // exitToolStripMenuItem
             // 
-            this.actionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.nextContourToolStripMenuItem});
-            this.actionsToolStripMenuItem.Name = "actionsToolStripMenuItem";
-            this.actionsToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
-            this.actionsToolStripMenuItem.Text = "Actions";
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
-            // nextContourToolStripMenuItem
+            // statusStrip1
             // 
-            this.nextContourToolStripMenuItem.Name = "nextContourToolStripMenuItem";
-            this.nextContourToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.nextContourToolStripMenuItem.Text = "Next Contour";
-            this.nextContourToolStripMenuItem.Click += new System.EventHandler(this.nextContourToolStripMenuItem_Click);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 537);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1012, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(86, 17);
+            this.toolStripStatusLabel1.Text = "No File Loaded";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 355F));
+            this.tableLayoutPanel1.Controls.Add(this.imageBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.imageBox2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.imageBox3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.imageBox4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.imageBox5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.imageBox6, 2, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1012, 513);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox1.Location = new System.Drawing.Point(3, 3);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(322, 250);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox2.Location = new System.Drawing.Point(331, 3);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(322, 250);
+            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // imageBox3
+            // 
+            this.imageBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox3.Location = new System.Drawing.Point(659, 3);
+            this.imageBox3.Name = "imageBox3";
+            this.imageBox3.Size = new System.Drawing.Size(350, 250);
+            this.imageBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox3.TabIndex = 2;
+            this.imageBox3.TabStop = false;
+            // 
+            // imageBox4
+            // 
+            this.imageBox4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox4.Location = new System.Drawing.Point(3, 259);
+            this.imageBox4.Name = "imageBox4";
+            this.imageBox4.Size = new System.Drawing.Size(322, 251);
+            this.imageBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox4.TabIndex = 2;
+            this.imageBox4.TabStop = false;
+            // 
+            // imageBox5
+            // 
+            this.imageBox5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageBox5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox5.Location = new System.Drawing.Point(331, 259);
+            this.imageBox5.Name = "imageBox5";
+            this.imageBox5.Size = new System.Drawing.Size(322, 251);
+            this.imageBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox5.TabIndex = 2;
+            this.imageBox5.TabStop = false;
+            // 
+            // imageBox6
+            // 
+            this.imageBox6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.imageBox6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox6.Location = new System.Drawing.Point(659, 259);
+            this.imageBox6.Name = "imageBox6";
+            this.imageBox6.Size = new System.Drawing.Size(350, 251);
+            this.imageBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageBox6.TabIndex = 2;
+            this.imageBox6.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1349, 851);
-            this.Controls.Add(this.lineImageBox);
-            this.Controls.Add(this.finalImageBox);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
+            this.ClientSize = new System.Drawing.Size(1012, 559);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip2;
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.finalImageBox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.lineImageBox)).EndInit();
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox6)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -109,7 +226,21 @@
         private Emgu.CV.UI.ImageBox lineImageBox;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem actionsToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem nextContourToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OpenToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private Emgu.CV.UI.ImageBox imageBox3;
+        private Emgu.CV.UI.ImageBox imageBox4;
+        private Emgu.CV.UI.ImageBox imageBox5;
+        private Emgu.CV.UI.ImageBox imageBox6;
     }
 }
 
--- 3Dekeystoner/Form1.cs
+++ 3Dekeystoner/Form1.cs
@@ -17,7 +17,12 @@ namespace _3Dekeystoner
 {
     public partial class Form1 : Form
     {
-        public Mat lineImage;
+        public Mat colorImage;
+        public Mat thresholdImage;
+        public Mat contourImage;
+        public Mat bigContourImage;
+        public Mat polyAproxImage;
+        public Mat finalImage;
         public VectorOfVectorOfPoint contours;
         public int contourCount;
 
@@ -28,7 +33,7 @@ namespace _3Dekeystoner
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            /*
             Mat colorImage = new Mat("C:\\Users\\Chiz\\Documents\\3D Projects\\Wizardry 8\\Front.jpg");
             Mat img = new Mat();
             CvInvoke.CvtColor(colorImage, img, ColorConversion.Bgr2Gray);
@@ -99,15 +104,105 @@ namespace _3Dekeystoner
             finalImageBox.Image = croped;
 
             this.Text = convexCOI[0].Size.ToString();
+            */
         }
 
-        private void lineImageBox_Click(object sender, EventArgs e)
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            Close();
         }
 
-        private void nextContourToolStripMenuItem_Click(object sender, EventArgs e)
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
         {
+            OpenFileDialog openJPG = new OpenFileDialog();
+            if(openJPG.ShowDialog()==DialogResult.OK)
+            {
+                colorImage = new Mat(openJPG.FileName);
+            }
 
+            imageBox1.Image = colorImage;
+            thresholdImage = new Mat();
+            
+            
+            CvInvoke.CvtColor(colorImage, thresholdImage, ColorConversion.Bgr2Gray);
+
+            UMat pyrDown = new UMat();
+            CvInvoke.PyrDown(thresholdImage, pyrDown);
+            CvInvoke.PyrUp(pyrDown, thresholdImage);
+            CvInvoke.Threshold(thresholdImage, thresholdImage, 100, 255, ThresholdType.BinaryInv);
+
+            imageBox2.Image = thresholdImage;
+            
+            contours = new VectorOfVectorOfPoint();
+            Mat m = new Mat();
+            contourImage = new Mat();
+            colorImage.CopyTo(contourImage);
+            contourImage.SetTo(new MCvScalar(0.0));
+
+            CvInvoke.FindContours(thresholdImage, contours, m, RetrType.Ccomp, ChainApproxMethod.ChainApproxSimple);
+            CvInvoke.DrawContours(contourImage, contours, -1, new MCvScalar(255.0,0.0,0.0), 2);
+
+            imageBox3.Image = contourImage;
+            
+            double largestArea = 0.0;
+            double tempArea = 0.0;
+            int largestContourIndex = 0;
+
+            for (int i = 0; i < contours.Size; i++)
+            {
+                tempArea = CvInvoke.ContourArea(contours[i]);
+                if (tempArea > largestArea)
+                {
+                    largestArea = tempArea;
+                    largestContourIndex = i;
+                }
+            }
+
+            bigContourImage = new Mat();
+            contourImage.CopyTo(bigContourImage);
+            bigContourImage.SetTo(new MCvScalar(0.0));
+            CvInvoke.DrawContours(bigContourImage, contours, largestContourIndex, new MCvScalar(255.0), 2);
+
+            imageBox4.Image = bigContourImage;
+
+
+            polyAproxImage = new Mat();
+            bigContourImage.CopyTo(polyAproxImage);
+            polyAproxImage.SetTo(new MCvScalar(255.0,255.0,255.0));
+
+            VectorOfVectorOfPoint convexCOI = new VectorOfVectorOfPoint(1);
+            CvInvoke.ConvexHull(contours[largestContourIndex], convexCOI[0]);
+            CvInvoke.ApproxPolyDP(convexCOI[0], convexCOI[0], 100, true);
+            CvInvoke.DrawContours(polyAproxImage, convexCOI, 0, new MCvScalar(0.0), 3);
+
+            imageBox5.Image = polyAproxImage;
+
+            Mat pMatrix = new Mat();
+            PointF[] temp = new PointF[] { new PointF(2048, 0), new PointF(2048, 2048), new PointF(0, 2048), new PointF(0, 0) };
+            finalImage = new Mat();
+            colorImage.CopyTo(finalImage);
+
+
+            if (convexCOI[0].Size == 4)
+            {
+                Point[] ctemp = convexCOI[0].ToArray();
+                PointF[] coit = new PointF[4];
+                for (int i = 0; i < 4; i++)
+                {
+                    coit[i].X = ctemp[i].X;
+                    coit[i].Y = ctemp[i].Y;
+                }
+
+                pMatrix = CvInvoke.GetPerspectiveTransform(coit, temp);
+                CvInvoke.WarpPerspective(finalImage, finalImage, pMatrix, colorImage.Size);
+            }
+
+            Mat croped = new Mat(finalImage, new Rectangle(0, 0, 2048, 2048));
+            CvInvoke.GetRotationMatrix2D(new Point(croped.Cols / 2, croped.Rows / 2), -90, 1, pMatrix);
+            CvInvoke.WarpAffine(croped, croped, pMatrix, croped.Size);
+
+            imageBox6.Image = croped;
         }
     }
 }
